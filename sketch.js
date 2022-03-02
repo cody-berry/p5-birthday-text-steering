b46dea4e9a43139f7e4b173b0da2f960957d66f6
Cody <cody.tian@icloud.com>
2022-03-02 14:50:37 -0800

    fix sound by commenting code, add touchStarted()

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 /*
 @author Cody
-@date 2021.09.16
+@date 2022.02.22
 
 version comments
 . program shell: basic setup, draw. css styling
@@ -16,7 +16,7 @@ version comments
  making the vehicles rainbow
 version comments
 ☒ editions: add color, display line
-☐ add edges()
+☒ add edges()
 ☐ add sound, sizes get bigger depending on p5.Beat, however it is calculated
 ☐ jiggling particles and lyrics with blue rectangle having right side going
  to the right starting with a rectangle with width 0, height textAscent() +
@@ -27,37 +27,89 @@ version comments
 let font
 let vehicles = []
 
+// here's our theme song for Liya's birthday: Daniel's Tiger Neighborhood!
+let dtnThemeSong
+let playing = false
+
+// the amount of milliseconds before the song started
+let songStartMillis
+
+
 function preload() {
-  font = loadFont('fonts/Meiryo-01.ttf');
+  font = loadFont('fonts/Meiryo-01.ttf')
+  dtnThemeSong = loadSound('dtnthemesong.mp3')
 }
+
 
 function setup() {
   createCanvas(600, 300);
   colorMode(HSB, 360, 100, 100, 100);
-  strokeWeight(2)
+  strokeWeight(4)
   textSize(130)
-  let points = font.textToPoints('Happy birthday Liya!', 10, height/2, 50, {sampleFactor: 0.5})
+
+  let points = font.textToPoints(
+      'Happy birthday Liya!', 10, height/2, 50,
+      {sampleFactor: 0.3, simplifyThreshold: 0})
+
   for (let i = 0; i < points.length; i++){
-    vehicles.push(new Vehicle(points[i].x, points[i].y, color(map(points[i].x, 0, width-100, 0, 360), 100, 100)))
+    vehicles.push(
+        new Vehicle(
+            points[i].x, points[i].y,
+            color(map(points[i].x, 0, width-100, 0, 360), 100, 100),
+            round(random())))
   }
 }
 
+
 function draw() {
-  background(0, 0, 50);
+  background(234, 24, 34)
 
-  // make a line moving from right to left and whatever it has covered is shown
-  let showingPosition = width-frameCount*10
-  stroke(0, 0, 100)
-  line(showingPosition, 0, showingPosition, height)
+  // // make a line moving from right to left and whatever it has covered is shown
+  // let showingPosition = width-frameCount*10
+  // stroke(0, 0, 100)
+  // line(showingPosition, 0, showingPosition, height)
+  //
+  // for (let i = 0; i < vehicles.length; i++){
+  //   let vh = vehicles[i]
+  //
+  //   if (vh.pos.x >= showingPosition) {
+  //     vh.show()
+  //   }
+  //
+  //   vh.update()
+  //   // 45000 is the amount of milliseconds the song lasts for.
+  //   if (millis() - songStartMillis > 45000) {
+  //     vh.behaviors()
+  //     dtnThemeSong.stop()
+  //     console.log("Solute! Stopping.")
+  //     noLoop()
+  //   }
+  //
+  //   vh.edges()
+  // }
 
-  for (let i = 0; i < vehicles.length; i++){
-    let vh = vehicles[i]
+  // console.log(dtnThemeSong.isPlaying())
+}
 
-    if (vh.pos.x >= showingPosition) {
-      vh.show()
-    }
-    vh.update()
-    vh.behaviors()
-    vh.edges()
+
+
+
+
+function keyPressed() {
+  if (key === 's') {
+    // console.log(dtnThemeSong.volume())
+    console.log("the song is about to play!")
+    dtnThemeSong.play()
+    songStartMillis = millis()
+    playing = true
+  }
+
+  // console.log("the mouse has been pressed!")
+}
+
+
+function touchStarted() {
+  if (getAudioContext().state !== 'running') {
+    getAudioContext().resume().then(r => {});
   }
 }
--- vehicle.js
+++ vehicle.js
@@ -1,6 +1,6 @@
 
 
-function Vehicle(x, y, c){
+function Vehicle(x, y, c, inverted){
     this.pos = new p5.Vector(random(width), random(height))
     this.vel = p5.Vector.random2D()
     this.acc = new p5.Vector(0, 0)
@@ -11,6 +11,14 @@ function Vehicle(x, y, c){
 
     // our color
     this.c = c
+
+    // is our radius inverted (the amplitude maps to from 1 to 8 or from 8
+    // to 1)?
+    this.invert = inverted
+
+    // this.invert's description contains "the amplitude". Here, we'll
+    // define the amplitude we're talking about.
+    this.p5amp = new p5.Amplitude()
 }
 
 
@@ -25,9 +33,16 @@ Vehicle.prototype.show = function(){
 
 // updates the vehicle's position, velocity, and acceleration
 Vehicle.prototype.update = function(){
-   this.pos.add(this.vel)
-   this.vel.add(this.acc)
-   this.acc.mult(0)
+    this.pos.add(this.vel)
+    this.vel.add(this.acc)
+    this.acc.mult(0)
+
+    // update our r
+    this.r = map(this.p5amp.getLevel(), 0, 1, 1, 8)
+    if (this.invert) {
+        this.r = this.r - 8
+        this.r++
+    }
 }
 
 //applies a force to the vehicle
